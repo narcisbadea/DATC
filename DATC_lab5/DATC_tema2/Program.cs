6087f133485025afbe84c0756fc827b7ffea12bf
Narcis Badea <badeanarcis@gmail.ocm>
2022-10-30 16:40:00 +0200

    second commit

---
+++ DATC_lab5/DATC_tema2/Program.cs
@@ -0,0 +1,15 @@
+﻿using (var client = new HttpClient())
+{
+    client.BaseAddress = new Uri("https://datctema20221029173655.azurewebsites.net/");
+    //HTTP GET
+    var countStudentsTask = await client.GetAsync("students/count");
+
+    if (countStudentsTask.IsSuccessStatusCode)
+    {
+        var countStudents = await countStudentsTask.Content.ReadAsAsync<int>();
+
+        var postCount = await client.PostAsJsonAsync("/metric", countStudents);
+
+        Console.WriteLine(postCount.StatusCode);
+    }
+}
